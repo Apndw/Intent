e43f2ded51a755b29b27379b02cfd96293d1bb66
Benjamin4k <pandu300478@gmail.com>
2022-01-11 16:04:46 +0700

    Upload All File

---
+++ app/src/main/java/com/benjamin/xi_rpl3_apripandu_intent/DetailActivity.kt
@@ -0,0 +1,11 @@
+package com.benjamin.xi_rpl3_apripandu_intent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
